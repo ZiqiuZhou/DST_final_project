8d0a1b07e13242bb63032088b00fadee49ac427f
ZiqiuZhou <zzq1063748118@gmail.com>
2022-02-21 20:47:53 +0100

    create repo

---
+++ Data/Lorenz-63/Data/data_generation.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+project_dir = "/home/dietrich/final_project_LSTM"
+
+train_data_raw = np.load(project_dir + 'lorenz_train.npy')
+test_data_raw = np.load(project_dir + 'lorenz_test.npy')
+training_data = np.vstack(([data_segment for data_segment in train_data_raw]))
+testing_data = np.vstack(([data_segment for data_segment in test_data_raw]))
+
+# generate training data
+data_dict_train = {}
+data_dict_train['train_input_sequence'] = training_data
+data_dict_train['dt'] = 1
+np.save(project_dir + '/Data/Lorenz-63/Data/training_data_N100000.npy', data_dict_train)
+
+# generate testing data
+data_dict_test = {}
+data_dict_test['test_input_sequence'] = testing_data
+
+dl_max = 20000
+pl_max = 20000
+max_idx = len(testing_data) - pl_max
+min_idx = dl_max
+idx = np.arange(min_idx, max_idx)
+np.random.shuffle(idx)
+testing_ic_indexes = idx
+
+data_dict_test['testing_ic_indexes'] = testing_ic_indexes
+data_dict_test['dt'] = 1
+np.save(project_dir + '/Data/Lorenz-63/Data/testing_data_N100000.npy', data_dict_test)
